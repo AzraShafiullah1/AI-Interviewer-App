aaa1eca412ad41bb265f83c7d2b4db6e3e3bd22e
Azra Shafi Ullah <164544626+AzraShafiullah1@users.noreply.github.com>
2025-04-07 22:09:34 +0000

    This is third commit

--- ai-interviewer/app/dashboard/_components/AddNewinterview.jsx
+++ ai-interviewer/app/dashboard/_components/AddNewinterview.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 function AddNewInterview() {
     return (
-        <div>AddNewInterview</div>
+        <div>
+            <div className="p-10 border rounded-lg bg-secondary
+            hover:scale-105 hover:shadow-md cursor-pointer transition-all">
+                <h2 className="text-lg text-center">+ Add New</h2>
+            </div>
+        </div>
     )
 }
 
