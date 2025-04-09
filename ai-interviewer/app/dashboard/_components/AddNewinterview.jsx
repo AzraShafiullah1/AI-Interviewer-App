3ab6c9b7af6e642bc2e2b242894d06857278aced
Azra Shafi Ullah <164544626+AzraShafiullah1@users.noreply.github.com>
2025-04-09 22:41:12 +0000

    I created  AddNewinterview

--- ai-interviewer/app/dashboard/_components/AddNewinterview.jsx
+++ ai-interviewer/app/dashboard/_components/AddNewinterview.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState } from "react";
-import { Button } from "@/components/ui/button";
+
 
 import {
     AlertDialog,
@@ -11,12 +11,21 @@ import {
     AlertDialogTitle,
    
   } from "@/components/ui/alert-dialog"
-  
-
-
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 function AddNewInterview() {
     const [openDailog, setOpenDailog] =useState(false)
+    const [jobPosition,setJobPosition]=useState();
+    const [jobDesc,setJobDesc]=useState();
+    const [jobExperience,setJobExperience]=useState();
+     
+    const onSubmit=(e)=>{
+      e.preventDefault()
+      console.log(setJobPosition, jobDesc,jobExperience)
+
+    }
     return (
         <div>
             <div className="p-10 border rounded-lg bg-secondary
@@ -29,23 +38,44 @@ function AddNewInterview() {
 
             <AlertDialog open={openDailog}>
 
-          <AlertDialogContent>
+          <AlertDialogContent className="max-w-2xl">
                <AlertDialogHeader>
-              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogTitle className="text-2xl">Tell us more about your job interviewing</AlertDialogTitle>
       <AlertDialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-        <div className="flex gap-5 justify-end">
-        <Button variant="ghost" onClick={()=>setOpenDailog(false)}>Cancel</Button>
-        <Button>Start Interview</Button>
-           
-            
+        <form onSubmit={onSubmit}>
+      <div>
+     
+        <h2>Add Details about your job position/role, job description and years of experience</h2>
+
+        <div className="mt-7 my-3">
+        <label>Job Role/Job Position</label>
+        <Input placeholder="Ex. Full Stack Developer" required
+        onChange={(event)=>setJobPosition(event.target.value)}
+        />
 
         </div>
+        <div className="my-3">
+        <label>Job Description/ Tech Stack (In Short)</label>
+        <Textarea placeholder="Ex. Typescript, React, NextJs, NodeJs, MySql extc"
+           required
+           onChange={(event)=>setJobDesc(event.target.value)}
+        />
+        </div>
+        <div className="my-3">
+        <label>Years of experience</label>
+        <Input placeholder="Ex.5" type="number" max="100" 
+        required
+        onChange={(event)=>setJobExperience(event.target.value)}
+        />
+        </div>
+      </div>
+        <div className="flex gap-5 justify-end">
+        <Button type="button"  variant="ghost" onClick={()=>setOpenDailog(false)}>Cancel</Button>
+        <Button type="submit">Start Interview</Button>
+     </div>
+     </form>
       </AlertDialogDescription>
-       
-
-    </AlertDialogHeader>
+       </AlertDialogHeader>
      </AlertDialogContent>
     </AlertDialog>
 
